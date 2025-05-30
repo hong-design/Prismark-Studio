5b3016ad7e722b2befb5fd6b773fc2672cbb4029
hong-design <hongx3360@gmail.com>
2025-05-30 12:31:51 +0800

    ?芸??函蔡 2025/05/30 週五 12:31:51.75

--- deploy.bat
+++ deploy.bat
@@ -1,20 +1,17 @@
 @echo off
 echo === 開始部署 PrismarkStudio ===
 
-:: 記錄當前時間
-set time=%time%
-echo [%time%] 更新檔案狀態...
-
 :: 將變更加入 Git
+echo [INFO] 加入變更檔案...
 git add .
 
-:: 自動建立 commit，包含時間戳
+:: 自動建立 commit，包含動態時間戳
+echo [INFO] 建立 Commit...
 git commit -m "自動部署 %date% %time%"
 
-:: Push 到 GitHub
+:: 推送到 GitHub
+echo [INFO] 推送到 GitHub...
 git push origin main
 
-echo === 部署完成！網站已更新到 GitHub Pages ===
+echo === ✅ 部署完成！網站已更新到 GitHub Pages ===
 pause
-:: 等待使用者按任意鍵繼續
-:: 清理環境變數
